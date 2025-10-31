27c3730cc8aab82045cfd9e17be0739a988eceeb
DevPBDias <devpbdias@gmail.com>
2025-10-31 16:39:02 -0300

    setup: configuração inicial da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Layout } from './pages/layout/layout';
+import { Hero } from './pages/hero/hero';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Hero,
+      },
+    ],
+  },
+];
