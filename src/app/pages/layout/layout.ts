811da06921c13c7649400f926ac41075e3aa7907
DevPBDias <devpbdias@gmail.com>
2025-10-31 17:30:04 -0300

    feat: adicionei o bg-image para o layout e criei o hero component para a pagina inicial

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { RouterOutlet } from '@angular/router';
+import { BackgroundImage } from '../../components/background-image/background-image';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, BackgroundImage],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
